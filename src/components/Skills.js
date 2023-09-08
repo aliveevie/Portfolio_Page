28aa1e3e4c59662ff5ae84a6cf0f68c886f31657
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-08 11:14:20 +0100

    feat: added more skills.js

---
+++ src/components/Skills.js
@@ -0,0 +1,35 @@
+export function Skills(){
+    return (
+        <div>
+            <h1>This is an array of my skills</h1>
+            <img  
+            src="#"
+            alt="html logo"
+            />
+             <img  
+            src="#"
+            alt="javascript logo"
+            />
+             <img  
+            src="#"
+            alt="python logo"
+            />
+            <img  
+            src="#"
+            alt="css logo"
+            />
+              <img  
+            src="#"
+            alt="sql logo"
+            />
+            <img  
+            src="#"
+            alt="mongoDB logo"
+            />
+            <img  
+            src="#"
+            alt="passport js logo"
+            />
+        </div>
+    )
+}
