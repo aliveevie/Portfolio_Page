af1470730ef134a334915d537fbfd2143ab328ed
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-08 16:30:31 +0100

    feat: modified src/components files

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,22 +9,23 @@ export function Header() {
           <a href="resume">Resume</a>
           <a href="skills">Skills</a>
           <a href="contact">Contact</a>
-       
+        </nav>
         <div className="header-icons">
-          <a href="#">
+          <a href="https://github.com/aliveevie/">
             <img src="/github.svg" />
           </a>
-          <a href="#">
+          <a href="https://twitter.com/iabdulkarim472">
             <img src="/twitter.svg"  />
           </a>
-          <a href="#">
-            <img src="/whatapp.svg"  />
+          <a href="tel:+2348138300357">
+            <img src="/whatapp.svg" alt="WhatsApp" />
           </a>
-          <a href="#">
-            <img src=""  />
+
+          <a href="https://www.linkedin.com/in/ibrahim193/">
+          <img src="/linkedIn.svg"  />
           </a>
+        
         </div>
-        </nav>
       </div>
     );
   }
