49c142a8ac5af6220553c23a3117270b4f9c6a19
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-10-24 12:27:57 +0100

    Update: many files

--- src/components/Webapp.js
+++ src/components/Webapp.js
@@ -11,18 +11,77 @@ export function Webapp() {
             </div>
             <div className="apps">
                 <div className="app1">
-                    <div className="line"></div>
                     <div className='textandline' >
                     <div className="number">1</div>
+                    <div className="line"></div>
                     <div className="text">
                         <h3>Smart-T</h3>
                         <p>
                             Revolutionizing Education, One App at a Time:
                             Crafting Apps to Transform Teaching in Schools
                         </p>
+                        <div className='buttons' >
+                            <button>Learn More</button>
+                            <button>Source Code</button>
+                            <button>Live Url</button>
+                        </div>
+                       
                     </div>
                     </div>
-                    
+                </div>
+                <div className="app2">
+                    <div className='textandline' >
+                    <div className="text">
+                        <h3>Game of Life</h3>
+                        <p>
+                        Breathing Life into Games: 
+                        I Create the Magic Behind the World of Gaming
+                        </p>
+                        <div className='buttons' >
+                            <button>Learn More</button>
+                            <button>Source Code</button>
+                            <button>Live Url</button>
+                        </div>
+                    </div>
+                    <div className="number">2</div>
+                    <div className="line"></div>
+                    </div>
+                </div>
+                <div className="app1">
+                    <div className='textandline' >
+                    <div className="number">3</div>
+                    <div className="line"></div>
+                    <div className="text">
+                        <h3>Blog-Post-API</h3>
+                        <p>
+                        Unlocking the Art of Storytelling: 
+                        My API Journey in the World of Blog Posts!
+                        </p>
+                        <div className='buttons' >
+                            <button>Learn More</button>
+                            <button>Source Code</button>
+                            <button>Live Url</button>
+                        </div>
+                       
+                    </div>
+                    </div>
+                </div>
+                <div className="app2">
+                    <div className='textandline' >
+                    <div className="text">
+                        <h3>Tic-Tae-Toe Game</h3>
+                        <p>
+                        X's and O's in Battle: Crafting the Ultimate Tic-Tac-Toe Experience
+                        </p>
+                        <div className='buttons' >
+                            <button>Learn More</button>
+                            <button>Source Code</button>
+                            <button>Live Url</button>
+                        </div>
+                    </div>
+                    <div className="number">4</div>
+                    <div className="line"></div>
+                    </div>
                 </div>
             </div>
         </div>
