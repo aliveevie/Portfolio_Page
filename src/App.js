8079334449bdc94b8a7e01e24dc5bce1fdb83537
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-10-24 11:09:17 +0100

    Update: modified many files

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Projects } from './components/Projects';
 import { Skills } from './components/Skills';
 import { Contact } from './components/Contact';
 import { Content } from './components/Content';
+import { Webapp } from './components/Webapp';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Content></Content>
+      <Webapp></Webapp>
     </div>
   );
 }
---
+++ src/components/Webapp.js
@@ -0,0 +1,30 @@
+import '../styles/webapp.css'
+
+export function Webapp() {
+    return (
+        <div className="webapp">
+            <h1 className="webapp-heading">&lt;Web Apps /&gt;</h1>
+            <div className="webanimation">
+                <p className="hidden-text">
+                    Unlocking the Power of Web Apps: Crafting the Future with Captivating Applications
+                </p>
+            </div>
+            <div className="apps">
+                <div className="app1">
+                    <div className="line"></div>
+                    <div className='textandline' >
+                    <div className="number">1</div>
+                    <div className="text">
+                        <h3>Smart-T</h3>
+                        <p>
+                            Revolutionizing Education, One App at a Time:
+                            Crafting Apps to Transform Teaching in Schools
+                        </p>
+                    </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
