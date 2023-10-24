5ec8e1dd23fc9caf54f5d121e30ae4633ee65ed3
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-10-24 14:21:56 +0100

    Update files

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,47 +4,31 @@ export function Skills() {
   return (
     <div id="skills" className="skills-container">
       <div className='skill-header' >
-            <h2>&lt;Skills /&gt;</h2>
-            <p>This is an array of my expetise set</p>
+        <h2>&lt;Skills /&gt;</h2>
       </div>
-      <div className="skills-images">
-        <div className="skill">
-          <img src="/html.svg" alt="HTML Logo" />
-         
-        </div>
-        <div className="skill">
-          <img src="/coding.svg" alt="JavaScript Logo" />
-        
-        </div>
-        <div className="skill">
-          <img src="/python.svg" alt="Python Logo" />
-          
-        </div>
-        <div className="skill">
-          <img src="/css.svg" alt="CSS Logo" />
-        
-        </div>
-        <div className="skill">
-          <img src="/sql.svg" alt="SQL Logo" />
-        
-        </div>
-        <div className="skill">
-          <img src="/mongoDB.svg" alt="MongoDB Logo" />
-          
-        </div>
-        <div className="skill">
-          <img src="/react.svg" alt="Passport.js Logo" />
-        
-        </div>
-        <div className="skill">
-          <img src="/node.svg" alt="Passport.js Logo" />
-        
-        </div>
-        <div className="skill">
-          <img src="/linux.svg" alt="Passport.js Logo" />
-        
-        </div>
-      </div>
+     <div class="icon-grid">
+        <i class="fab fa-html5"></i>
+        <i class="fab fa-css3"></i>
+        <i class="fab fa-js"></i>
+        <i class="fab fa-python"></i>
+        <i class="fab fa-node"></i>
+        <i class="fab fa-react"></i>
+        <i class="fab fa-java"></i>
+        <i class="fab fa-php"></i>
+        <i class="fab fa-database"></i>
+        <i class="fab fa-linux"></i>
+        <i class="fab fa-android"></i>
+        <i class="fab fa-apple"></i>
+        <i class="fab fa-git"></i>
+        <i class="fab fa-github"></i>
+        <i class="fab fa-gitlab"></i>
+        <i class="fab fa-docker"></i>
+        <i class="fab fa-aws"></i>
+        <i class="fab fa-react"></i>
+        <i class="fab fa-angular"></i>
+        <i class="fab fa-vuejs"></i>
+</div>
+
     </div>
   );
 }
